0aa4d2861b9d6eb1f55556f4622435d427bc8432
muhammadirfanabil <muhammadirfanabil@gmail.com>
2025-05-07 23:11:53 +0700

    mapdonelast

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'models/User/UserInformation.dart';
 import '../firebase_options.dart';
-import 'screens/Users/Index.dart';
-import '../screens/Auth/Login.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'widgets/MapWidget.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,30 +19,19 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
-      routes: {
-        '/': (context) => const OSMFlutterMap(),
-        // '/map': (context) => const OSMFlutterMap(),
-      },
+      routes: {'/': (context) => const GPSMapScreen()},
     );
   }
 }
 
-class OSMFlutterMap extends StatelessWidget {
-  const OSMFlutterMap({super.key});
+class GPSMapScreen extends StatelessWidget {
+  const GPSMapScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Peta OpenStreetMap")),
-      body: FlutterMap(
-        options: MapOptions(),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.example.gps_app',
-          ),
-        ],
-      ),
+      body: const MapWidget(),
     );
   }
 }
